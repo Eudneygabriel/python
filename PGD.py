5aeece96046158044c8d6bcb6295dfed81b11ed3
Elson Mauricio <151577129+Maelson11@users.noreply.github.com>
2024-11-07 13:55:07 +0000

    correção de erro

--- PGD.py
+++ PGD.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def pagina9():
     return render_template('PGD.html')
 
+@app.route('/imagem-grande/<nome_imagem>')
+def imagem_grande(nome_imagem):
+    # Renderiza uma página que mostra a imagem em tamanho grande
+    return render_template('imagem_grande.html', nome_imagem=nome_imagem)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
